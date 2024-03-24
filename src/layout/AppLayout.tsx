eb63989e632808751dae9974d2f520e7dd10d5ca
Bikcode <bikcodeh@gmail.com>
2024-03-24 10:51:43 -0500

    add headless ui and add navmenu component

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import { Logo } from "@/components"
+import { Logo, NavMenu } from "@/components"
 
 export const AppLayout = () => {
     return (
@@ -9,6 +9,7 @@ export const AppLayout = () => {
                     <div className="w-64">
                         <Logo />
                     </div>
+                    <NavMenu />
                 </div>
             </header>
             <section className="max-w-screen-2xl mx-auto mt-10 p-5">
