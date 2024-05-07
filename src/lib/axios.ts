f98a91e69e8801868beef6004a127ee5cde0892b
Bikcode <bikcodeh@gmail.com>
2024-05-07 06:27:16 -0500

    create interceptor for token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,3 +3,11 @@ import axios from "axios";
 export const uptaskApi = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
+
+uptaskApi.interceptors.request.use((config) => {
+    const token = localStorage.getItem('AUTH_TOKEN');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config
+})
